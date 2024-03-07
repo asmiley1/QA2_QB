06f48efd491eef9920431ce3fafe29ff524979cb
asmiley1 <asmiley1404@gmail.com>
2024-03-07 09:13:30 -0600

    Created createTable file and added the names in of the tables

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # QA2_QB
 Quarterly Assessment over Quiz Bowl
+-createTable file allows you to create and add table into the database file.
---
+++ createTable.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+conn = sqlite3.connect('QuizBowl.db')
+cursor = conn.cursor()
+
+# Create tables for each category
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Financial_Management (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question TEXT,
+    answer TEXT
+);
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS DS_3850 (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question TEXT,
+    answer TEXT
+);
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Data_Analytics (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question TEXT,
+    answer TEXT
+);
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Database_Managment (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question TEXT,
+    answer TEXT
+);
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS BMGT_3510 (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question TEXT,
+    answer TEXT
+);
+''')
+# Repeat the above create table statement for each category
+
+conn.commit()
+conn.close()
